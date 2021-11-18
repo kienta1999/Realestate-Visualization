a210c9c1e1a73e3101368e9267e559e8e29d4f64
Anh Le <a.v.le@wustl.edu>
2021-11-18 03:02:35 -0600

    Added line chart for city house prices

--- js/lineChart.js
+++ js/lineChart.js
@@ -48,4 +48,49 @@ LineChart.prototype.displayLineChart = function () {
         .classed("state_cities_option", true)
         .attr("value", (d) => d.city)
         .html((d) => d.city);
+
+    dropdown.on("change", (event) => {
+        let selectedCity = dropdown.node().value;
+        let selectedData = cities_median_prices.find(d => d.city === selectedCity).price;
+
+        selectedData.forEach(d => {
+            d.date = d3.timeParse("%Y-%m-%d")(d.date);
+        });
+
+        console.log(selectedCity);
+        console.log(selectedData);
+
+        let xAxis = d3.scaleTime()
+            .range([ 0, self.svgWidth ])
+            .domain(d3.extent(selectedData, (d) => d.date))
+            
+        let yAxis = d3.scaleLinear()
+            .range([ self.svgHeight, 30 ])
+            .domain([0, d3.max(selectedData, (d) => d.price)])
+
+        self.svg.selectAll('.x-axis').remove();
+        self.svg.selectAll('.y-axis').remove();
+        self.svg.selectAll('.price-line').remove();
+
+        self.svg.append("g")
+            .attr("transform", "translate(0," + self.svgHeight - 40 + ")")
+            .attr("class", "x-axis")
+            .call(d3.axisBottom(xAxis))
+
+        self.svg.append("g")
+            .attr("class", "y-axis")
+            .call(d3.axisLeft(yAxis))
+      
+        // Add the line
+        self.svg.append("path")
+            .datum(selectedData)
+            .attr("fill", "none")
+            .attr("class", "price-line")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function(d) { return xAxis(d.date) })
+                .y(function(d) { return yAxis(d.price) })
+            )
+    });
 };
